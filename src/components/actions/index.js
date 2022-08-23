bfa57f726c57331ba4c13b9df6c95dd7e9f9b4af
yakubalkis <yakupalkiis@gmail.com>
2022-08-23 18:23:00 +0300

    Added fetch action for fetching countries from API

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -1,3 +1,12 @@
+import axios from "axios"
+
 export const toggle = () => dispatch => {
    dispatch({type: 'TOGGLE'})
+}
+
+export const getCountries = () => dispatch => {
+   axios
+   .get("https://restcountries.com/v2/all")
+   .then(response => dispatch({type: 'GET_COUNTRIES_SUCCESS', payload: response.data}))
+   
 }
