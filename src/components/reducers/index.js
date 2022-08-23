5f91d8488abc42096eaf328d1fab64804d7727fe
yakubalkis <yakupalkiis@gmail.com>
2022-08-23 18:23:31 +0300

    Added fetxh func's reducer.

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,11 +1,15 @@
 const INITIAL_STATE = {
-    isToggle:false
+    isToggle:false,
+    countries:[],
+
 }
 
 export const reducer = (state=INITIAL_STATE,action) => {
     switch(action.type){
         case 'TOGGLE':
             return {...state, isToggle:!state.isToggle}
+        case 'GET_COUNTRIES_SUCCESS':
+            return {...state, countries: action.payload}
         default:
             return state
     }
