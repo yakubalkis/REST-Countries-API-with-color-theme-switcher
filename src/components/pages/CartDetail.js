c6d200d2766be93f525036ac7aa28f5d11709ecd
yakubalkis <yakupalkiis@gmail.com>
2022-08-24 13:36:30 +0300

    Made dynamic page on CartDetail.

--- src/components/pages/CartDetail.js
+++ src/components/pages/CartDetail.js
@@ -1,38 +1,45 @@
 import React from "react";
 import { connect } from "react-redux";
 import { toggle } from "../actions";
+import { getId } from "../actions";
+import { Link } from "react-router-dom";
 import leftArrowLight from '../img/leftArrowLight.png'
 import leftArrowDark from '../img/leftArrowDark.png'
 
 function CartDetail(props){
+    console.log(props)
     const mode = props.isToggle ? 'dark' : 'light'
-    const icon = props.isToggle ? leftArrowDark : leftArrowLight
+    const icon = props.isToggle ? leftArrowDark : leftArrowLight 
+    const style = { boxShadow: props.isToggle ? '0 0 7px black' : '0 0 7px gray'}
     return (
         <div className={`cartDetail ${mode}-theme`}>
-            <div className={`btnIconDiv ${mode}-themeForHeader`} onClick={() => console.log('heloo')} >
-                <img className="btnIcon" alt="" src={icon} />
-                <button className={`backBtn ${mode}-themeForHeader `}>Back</button>
-            </div>
+
+           <Link to='/' style={{textDecoration:"none"}} > 
+                <div style={style} className={`btnBackDiv ${mode}-themeForHeader`}  >
+                    <img className="btnIcon" alt="" src={icon} />
+                    <button className={`backBtn ${mode}-themeForHeader `}>Back</button>
+                </div>
+            </Link>
             <div className="detail">
-                <div className="detail-flag"><img alt=""/></div>
+                <img className="detail-flag" alt="" src={props.countries[props.id].flag || props.countries[props.id].flags.png} />
                 <div className="detail-info">
-                    <div className="info-header">Belgium</div>
+                    <div className="info-header"><h2>{props.countries[props.id].name}</h2></div>
                     <div className="info-mid">
                         <div className="info-mid-left" >
-                            <p>Native Name: <span>Belgie</span></p>
-                            <p>Population: <span>11.319.511</span></p>
-                            <p>Region: <span>Europe</span></p>
-                            <p>Sub Region: <span>Western Europe</span></p>
-                            <p>Capital <span>Brussels</span></p>
+                            <p>Native Name: <span>{props.countries[props.id].nativeName}</span></p>
+                            <p>Population: <span>{props.countries[props.id].population}</span></p>
+                            <p>Region: <span>{props.countries[props.id].region}</span></p>
+                            <p>Sub Region: <span>{props.countries[props.id].subregion}</span></p>
+                            <p>Capital: <span>{props.countries[props.id].capital}</span></p>
                         </div>
                         <div className="info-mid-right" >
-                            <p>Top Level Domain: <span>.be</span></p>
-                            <p>Currencies: <span>Euro</span></p>
-                            <p>Languages: <span>Dutch, French, German</span></p>
+                            <p>Top Level Domain: <span>{props.countries[props.id].topLevelDomain}</span></p>
+                            <p>Currencies: <span>{props.countries[props.id].currencies[0].name}</span></p>
+                            <p>Languages: <span>{props.countries[props.id].languages.map(item => item.name)}</span></p>
                         </div>
-                        <div info-end>
-                            <p>Border Countries</p>
-                        </div>
+                    </div>
+                    <div className="info-end">
+                            <p>Border Countries:</p>
                     </div>
                 </div>
             </div>
@@ -42,7 +49,9 @@ function CartDetail(props){
 
 const mapStateToProps = state => {
     return{
-        isToggle:state.isToggle
+        countries:state.countries,
+        isToggle:state.isToggle,
+        id: state.id
     }
 }
 export default connect(mapStateToProps, {toggle})(CartDetail)
