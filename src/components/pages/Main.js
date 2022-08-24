c31ffae3c399eed2bab9ea4feb3327514fc1fe92
yakubalkis <yakupalkiis@gmail.com>
2022-08-24 17:26:58 +0300

    Added functionality to filter section on page.

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -9,34 +9,52 @@ import downArrowDark from '../img/downArrowDark.png'
 import { toggle } from "../actions";
 import { connect } from "react-redux";
 import { getCountries } from "../actions";
+import { filterCountries } from "../actions";
+
 
 function Main(props){
 
    const [arrowIcon, setArrowIcon] = useState()
-    const mode = props.isToggle ? 'dark' : 'light'
-    const searchIcon = props.isToggle ? darkSearchIcon: lightSearchIcon
-    const [isHovered,setIshovered] = useState(false)
+   const [isHovered,setIshovered] = useState(false)
+   const [region, setRegion] = useState('')
+   const mode = props.isToggle ? 'dark' : 'light'
+   const searchIcon = props.isToggle ? darkSearchIcon: lightSearchIcon
+    
 
     useEffect(() => {
         props.getCountries() // fetch data
+       
     },[])
 
     useEffect(() => {
        const icon = isHovered ? mode ==='dark' ? downArrowDark : downArrowLight : mode==='light' ? upArrowLight : upArrowDark
        setArrowIcon(icon)
     },[isHovered,mode])
-
+ 
+    useEffect(() => {
+        props.filterCountries(filteredRegion)
+    },[region])
+    
+    const filteredRegion = props.countries.filter((country) => country.region.toLowerCase().includes(region.toLocaleLowerCase()))
+    function handleSelect (event) { 
+        setRegion(event.target.value)
+        
+    }
+  
+    const Carts = filteredRegion.map((item,i) => {
+        return (
+          <Cart 
+                key={i}
+                idOf={i} 
+                src={item.flag} 
+                population={item.population} 
+                region={item.region} 
+                capital={item.capital} 
+                name = {item.name}
+          />
+            )
+    })
    
-    const Carts= props.countries.map((item,i) => (
-        <Cart 
-        key={i} 
-        src={item.flag} 
-        population={item.population} 
-        region={item.region} 
-        capital={item.capital} 
-        name = {item.name}
-        />
-    ))
     return(
         <main className={`main ${mode}-theme`}>
             <div className="search-dropdown">
@@ -47,16 +65,16 @@ function Main(props){
                 <div className={`dropdown ${mode}-themeForHeader`} onMouseEnter={() => setIshovered(true)} onMouseLeave={() => setIshovered(false)}>
                     <p className={`dropBtn ${mode}-themeForHeader`}>Filter by Region <img className="arrowIcon" alt="" src={arrowIcon} /></p>
                     <div className="dropdown-content">
-                        <a href="#">Africa</a>
-                        <a href="#">America</a>
-                        <a href="#">Asia</a>
-                        <a href="#">Europe</a>
-                        <a href="#">Oceania</a>
+                        <option value='Africa' onClick={(e) => handleSelect(e)}>Africa</option>
+                        <option value='Americas' onClick={(e) => handleSelect(e)} >America</option>
+                        <option value='Asia' onClick={(e) => handleSelect(e)}>Asia</option>
+                        <option value='Europe' onClick={(e) => handleSelect(e)}>Europe</option>
+                        <option value='Oceania' onClick={(e) => handleSelect(e)}>Oceania</option>
                     </div>
                 </div>
            </div>
            <div className="carts">
-           {Carts}
+            {Carts}
            </div>
             
         </main>
@@ -68,4 +86,4 @@ const mapStateToProps = state => {
         isToggle: state.isToggle
     }
 }
-export default connect(mapStateToProps,{getCountries, toggle})(Main)
+export default connect(mapStateToProps,{getCountries, toggle, filterCountries})(Main)
