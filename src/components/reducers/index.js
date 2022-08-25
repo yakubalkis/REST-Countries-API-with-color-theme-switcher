68685e2aaeeffa26209f6f12d288fee0724a9f43
yakubalkis <yakupalkiis@gmail.com>
2022-08-25 20:35:47 +0300

    Added a reducer for displaying 'loading' gif

--- src/components/reducers/index.js
+++ src/components/reducers/index.js
@@ -1,15 +1,26 @@
 const INITIAL_STATE = {
+    isLoading:false,
     isToggle:false,
     countries:[],
-
+    id:'',
+    nameOf:'',
+    filteredCountries:[]
 }
 
 export const reducer = (state=INITIAL_STATE,action) => {
     switch(action.type){
         case 'TOGGLE':
             return {...state, isToggle:!state.isToggle}
+        case 'ID':
+            return {...state, id: action.payload}
+        case 'GET_COUNTRIES_START':
+            return {...state, isLoading:true}
         case 'GET_COUNTRIES_SUCCESS':
-            return {...state, countries: action.payload}
+            return {...state, countries: action.payload, isLoading:false}
+        case 'FILTER':
+            return {...state, filteredCountries: action.payload}
+        case 'GET_NAME':
+            return {...state, nameOf: action.payload }
         default:
             return state
     }
