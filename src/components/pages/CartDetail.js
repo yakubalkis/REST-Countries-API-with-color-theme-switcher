b4db21effcf0f746d829151fc73b2b22a026c5a2
yakubalkis <yakupalkiis@gmail.com>
2022-08-25 13:00:19 +0300

    Fixed bugs in component.

--- src/components/pages/CartDetail.js
+++ src/components/pages/CartDetail.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
-import { toggle } from "../actions";
-import { getId } from "../actions";
+import { toggle, filterCountries } from "../actions";
 import { Link } from "react-router-dom";
 import leftArrowLight from '../img/leftArrowLight.png'
 import leftArrowDark from '../img/leftArrowDark.png'
 
 function CartDetail(props){
-    console.log(props)
+    const filtered = props.filteredCountries.filter(country => country.name === props.nameOf)[0]
+    console.log(props.filteredCountries.filter(country => country.name === props.nameOf)[0])
     const mode = props.isToggle ? 'dark' : 'light'
     const icon = props.isToggle ? leftArrowDark : leftArrowLight 
     const style = { boxShadow: props.isToggle ? '0 0 7px black' : '0 0 7px gray'}
+    
     return (
         <div className={`cartDetail ${mode}-theme`}>
 
@@ -21,21 +22,21 @@ function CartDetail(props){
                 </div>
             </Link>
             <div className="detail">
-                <img className="detail-flag" alt="" src={props.countries[props.id].flag || props.countries[props.id].flags.png} />
+                <img className="detail-flag" alt="" src={filtered.flag} />
                 <div className="detail-info">
-                    <div className="info-header"><h2>{props.countries[props.id].name}</h2></div>
+                    <div className="info-header"><h2>{filtered.name}</h2></div>
                     <div className="info-mid">
                         <div className="info-mid-left" >
-                            <p>Native Name: <span>{props.countries[props.id].nativeName}</span></p>
-                            <p>Population: <span>{props.countries[props.id].population}</span></p>
-                            <p>Region: <span>{props.countries[props.id].region}</span></p>
-                            <p>Sub Region: <span>{props.countries[props.id].subregion}</span></p>
-                            <p>Capital: <span>{props.countries[props.id].capital}</span></p>
+                            <p>Native Name: <span>{filtered.nativeName}</span></p>
+                            <p>Population: <span>{filtered.population}</span></p>
+                            <p>Region: <span>{filtered.region}</span></p>
+                            <p>Sub Region: <span>{filtered.subregion}</span></p>
+                            <p>Capital: <span>{filtered.capital}</span></p>
                         </div>
                         <div className="info-mid-right" >
-                            <p>Top Level Domain: <span>{props.countries[props.id].topLevelDomain}</span></p>
-                            <p>Currencies: <span>{props.countries[props.id].currencies[0].name}</span></p>
-                            <p>Languages: <span>{props.countries[props.id].languages.map(item => item.name)}</span></p>
+                            <p>Top Level Domain: <span>{filtered.topLevelDomain}</span></p>
+                            <p>Currencies: <span>{filtered.currencies[0].name}</span></p>
+                            <p>Languages: <span>{filtered.languages.map(item => item.name)}</span></p>
                         </div>
                     </div>
                     <div className="info-end">
@@ -51,7 +52,9 @@ const mapStateToProps = state => {
     return{
         countries:state.countries,
         isToggle:state.isToggle,
-        id: state.id
+        id: state.id,
+        filteredCountries: state.filteredCountries,
+        nameOf: state.nameOf
     }
 }
-export default connect(mapStateToProps, {toggle})(CartDetail)
+export default connect(mapStateToProps, {toggle, filterCountries})(CartDetail)
