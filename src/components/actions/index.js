b91e633baf444d07a301f9d7189db341fdd19362
yakubalkis <yakupalkiis@gmail.com>
2022-08-25 15:15:29 +0300

    Actions built.

--- src/components/actions/index.js
+++ src/components/actions/index.js
@@ -8,5 +8,16 @@ export const getCountries = () => dispatch => {
    axios
    .get("https://restcountries.com/v2/all")
    .then(response => dispatch({type: 'GET_COUNTRIES_SUCCESS', payload: response.data}))
-   
+}
+
+export const getId = (id)  => dispatch => {
+   dispatch({type: 'ID', payload: id})
+}
+
+export const filterCountries = (arrayOfCountries) => dispatch => {
+   dispatch({type: 'FILTER', payload: arrayOfCountries})
+}
+
+export const getName = (name) => dispatch => {
+   dispatch({type: 'GET_NAME', payload: name})
 }
